652de05e76cf440359baf60b0ca6890bdde3cc03
Imbanga <blackthorn101@outlook.com>
2023-11-30 18:31:54 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# Sample Hardhat Project
+A blockchain is a globally shared, transactional database. This means that everyone can read entries in the database just by participating in the network. If you want to change something in the database, you have to create a so-called transaction which has to be accepted by all others.
+As an example, imagine a table that lists the balances of all accounts in an electronic currency. If a transfer from one account to another is requested, the transactional nature of the database ensures that if the amount is subtracted from one account, it is always added to the other account. If due to whatever reason, adding the amount to the target account is not possible, the source account is also not modified.
 
-This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+The Ethereum Virtual Machine or EVM is the runtime environment for smart contracts in Ethereum. It is not only sandboxed but actually completely isolated, which means that code running inside the EVM has no access to network, filesystem or other processes. Smart contracts even have limited access to other smart contracts.
 
-Try running some of the following tasks:
+There are two kinds of accounts in Ethereum which share the same address space: External accounts that are controlled by public-private key pairs (i.e. humans) and contract accounts which are controlled by the code stored together with the account.
 
-```shell
-npx hardhat help
-npx hardhat test
-REPORT_GAS=true npx hardhat test
-npx hardhat node
-npx hardhat run scripts/deploy.js
+A transaction is a message that is sent from one account to another account (which might be the same or empty, see below). It can include binary data (which is called “payload”) and Ether.
+If the target account contains code, that code is executed and the payload is provided as input data.
+
+
+The Ethereum Virtual Machine has three areas where it can store data: storage, memory and the stack.
+
 ```
--- contracts/example2.sol
+++ contracts/example2.sol
@@ -26,3 +26,37 @@ contract Coin {
         emit Sent(msg.sender, receiver, amount)
     }
 }
+
+// Breakdown of the above concepts
+// The line address public minter; declares a state variable of type address. 
+// The address type is a 160-bit value that does not allow any arithmetic operations.
+// The keyword public automatically generates a function that allows you to access the current value of the state variable from outside of the contract.
+
+// The next line, mapping(address => uint) public balances; also creates a public state variable, but it is a more complex datatype. 
+// The mapping type maps addresses to unsigned integers.
+
+// The constructor is a special function that is executed during the creation of the contract and cannot be called afterwards. 
+// In this case, it permanently stores the address of the person creating the contract. 
+// The msg variable (together with tx and block) is a special global variable that contains properties which allow access to the blockchain.
+//  msg.sender is always the address where the current (external) function call came from.
+
+// The line event Sent(address from, address to, uint amount); declares an “event”, 
+// which is emitted in the last line of the function send. 
+// Ethereum clients such as web applications can listen for these events emitted on the blockchain without much cost. 
+// As soon as it is emitted, the listener receives the arguments from, to and amount, which makes it possible to track transactions.
+
+// The mint function sends an amount of newly created coins to another address. 
+// The require function call defines conditions that reverts all changes if not met.
+//  In this example, require(msg.sender == minter); ensures that only the creator of the contract can call mint. 
+//  In general, the creator can mint as many tokens as they like, but at some point, this will lead to a phenomenon called “overflow”.
+
+// Errors allow you to provide more information to the caller about why a condition or operation failed.
+//  Errors are used together with the revert statement. 
+//  The revert statement unconditionally aborts and reverts all changes.
+//  it is similar to the require function, but it also allows you to provide the name of an error and additional data,
+//   which will be supplied to the caller (and eventually to the front-end application or block explorer) so that a failure can more easily be debugged or reacted upon.
+
+
+// The send function can be used by anyone (who already has some of these coins) to send coins to anyone else. 
+// If the sender does not have enough coins to send, the if condition evaluates to true. 
+// As a result, the revert will cause the operation to fail while providing the sender with error details using the InsufficientBalance error.
